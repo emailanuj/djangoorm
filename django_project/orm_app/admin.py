1d03524613cd0f71c5f7b9812369e4c5039a304b
emailanuj <emai.anujdubey@gmail.com>
2019-12-27 23:35:25 +0530

    Many-To-One Relationship

--- django_project/orm_app/admin.py
+++ django_project/orm_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Publication,Article
+from .models import Publication,Article,Reporter,News
 
 admin.site.register(Publication)
 admin.site.register(Article)
+admin.site.register(Reporter)
+admin.site.register(News)
--- django_project/orm_app/models.py
+++ django_project/orm_app/models.py
@@ -21,3 +21,23 @@ class Article(models.Model):
 
     class Meta:
         ordering=('headline',)
+
+class Reporter(models.Model):
+    first_name=models.CharField(max_length=30)
+    last_name=models.CharField(max_length=30)
+    email=models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+    
+class News(models.Model):
+    headline=models.CharField(max_length=100)
+    pub_date=models.DateField()
+    reporter=models.ForeignKey(Reporter,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.headline
+
+    class Meta:
+        ordering=('headline',)
+    
